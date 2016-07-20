f5d65726502c2ce6483172670a9f9dc226840eb8
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-07-19 23:05:30 -0400

    Updated version

--- src/main/java/org/abendigo/OffsetManager.java
+++ src/main/java/org/abendigo/OffsetManager.java
@@ -1,21 +1,18 @@
 package org.abendigo;
 
 import com.beaudoin.jmm.process.Module;
-import com.beaudoin.jmm.process.NativeProcess;
+import com.beaudoin.jmm.process.Processes;
 import com.sun.jna.Platform;
-import org.abendigo.misc.PatternScanner;
 import org.abendigo.netvars.NetVars;
 import org.abendigo.offsets.Offsets;
-
-import static org.abendigo.misc.PatternScanner.READ;
 
 /**
  * Created by Jonathan on 12/22/2015.
  */
 public final class OffsetManager {
 
-    private static NativeProcess process;
-    private static Module clientModule, engineModule;
+	private static Process process;
+	private static Module clientModule, engineModule;
 
     static {
         StringBuilder procBaseName = new StringBuilder("csgo");
@@ -30,10 +27,6 @@ public final class OffsetManager {
             procBaseName.append("_linux");
             clientBaseName.append("_client.so");
             engineBaseName.append("_client.so");
-
-            //\xE8\x00\x00\x00\x00\x8B\x78\x14\x6B\xD6\x38
-            int foundGlowPointerCall = PatternScanner.getAddressForPattern(clientModule, 0, 0, READ, 0xE8, 0x00, 0x00, 0x00, 0x00, 0x8B, 0x78, 0x14, 0x6B, 0xD6, 0x34);
-            System.out.println(foundGlowPointerCall);
         } else if (Platform.isMac()) {
             procBaseName.append("_osx");
             clientBaseName.append(".dylib");//Not sure completely
@@ -46,8 +39,8 @@ public final class OffsetManager {
         String clientName = clientBaseName.toString();
         String engineName = engineBaseName.toString();
 
-        waitUntilFound("process", () -> (process = NativeProcess.byName(processName)) != null);
-        waitUntilFound("client module", () -> (clientModule = process.findModule(clientName)) != null);
+	    waitUntilFound("process", () -> (process = Processes.byName(processName)) != null);
+	    waitUntilFound("client module", () -> (clientModule = process.findModule(clientName)) != null);
         waitUntilFound("engine module", () -> (engineModule = process.findModule(engineName)) != null);
     }
 
@@ -64,8 +57,8 @@ public final class OffsetManager {
         Offsets.load();
     }
 
-    public static NativeProcess process() {
-        return process;
+	public static Process process() {
+		return process;
     }
 
     public static Module clientModule() {
