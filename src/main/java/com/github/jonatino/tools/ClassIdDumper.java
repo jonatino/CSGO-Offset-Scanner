9c51c0f3a764b573a357bdf9612dbfbd28904805
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-08-29 22:32:26 -0400

    Updated m_dwGlobalVars offset

--- src/main/java/com/github/jonatino/tools/ClassIdDumper.java
+++ src/main/java/com/github/jonatino/tools/ClassIdDumper.java
@@ -24,28 +24,60 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by Jonathan on 1/1/2016.
  */
 public final class ClassIdDumper {
-
+	
 	public static void main(String[] args) {
 		int firstclass = PatternScanner.getAddressForPattern(OffsetManager.clientModule(), 0, 0, 0, "DT_TEWorldDecal");
 		firstclass = PatternScanner.getAddressForPattern(OffsetManager.clientModule(), 0x2B, 0, PatternScanner.READ, firstclass);
-
-
-		List<String> text = new ArrayList<>();
+		
+		
+		List<ClientClassInfo> text = new ArrayList<>();
 		ClientClass clientClass = new ClientClass();
 		for (clientClass.setBase(firstclass); clientClass.readable(); clientClass.setBase(clientClass.next())) {
-			text.add(clientClass.className() + " = " + clientClass.classId());
+			text.add(new ClientClassInfo(clientClass.className(), clientClass.classId()));
 		}
+		text.sort(new Comparator<ClientClassInfo>() {
+			@Override
+			public int compare(ClientClassInfo o1, ClientClassInfo o2) {
+				return Integer.compare(o1.id, o2.id);
+			}
+		});
+		List<String> lines = new ArrayList<>();
+		text.forEach(k -> lines.add(k.toString()));
 		try {
-			Files.write(Paths.get("ClassIds.txt"), text);
+			Files.write(Paths.get("ClassIds.txt"), lines);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static class ClientClassInfo {
+		
+		private String name;
+		private int id;
+		
+		public ClientClassInfo(String name, int id) {
+			this.name = name;
+			this.id = id;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder b = new StringBuilder();
+			b.append(name).append("(").append(id);
+			if ((name.startsWith("CWeapon") && !name.equals("CWeaponCubemap")&& !name.equals("CWeaponBaseItem")) || name.startsWith("CKnife") || name.equals("CAK47") || name.equals("CDEagle") || name.equals("CSCAR17")) {
+				b.append(", weapon = true");
+			} else if (name.startsWith("CBaseCSGrenade") || name.startsWith("CBaseGrenade") || name.startsWith("CDecoy") || name.startsWith("CMolotov") || name.startsWith("CSmokeGrenade") || name.equals("ParticleSmokeGrenade") || name.equals("CFlashbang") || name.equals("CHEGrenade") || name.equals("CIncendiaryGrenade") || name.equals("CInferno")) {
+				b.append(", grenade = true");
+			}
+			b.append("),");
+			return b.toString();
+		}
+	}
 }
